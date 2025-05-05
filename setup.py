c24c0daf2a5e0c2a043cdee65762501a8d619051
Nicolas Feyer <n.feyer@neur-on.ai>
2025-05-05 06:54:46 +0200

    feat: release version 1.0.0

--- setup.py
+++ setup.py
@@ -1,27 +1,28 @@
+import subprocess
+from pathlib import Path
+
 from setuptools import setup, find_packages
 from setuptools.command.build_ext import build_ext
-from pathlib import Path
-import subprocess
 
 
 class BuildFastAlign(build_ext):
-    def run(self):
-        build_dir = Path("external/fast_align/build")
-        build_dir.mkdir(parents=True, exist_ok=True)
+  def run(self):
+    build_dir = Path("external/fast_align/build")
+    build_dir.mkdir(parents=True, exist_ok=True)
 
-        # Run CMake configuration
-        subprocess.check_call(["cmake", ".."], cwd=build_dir)
-        # Build fast_align binary
-        subprocess.check_call(["make"], cwd=build_dir)
+    # Run CMake configuration
+    subprocess.check_call(["cmake", ".."], cwd=build_dir)
+    # Build fast_align binary
+    subprocess.check_call(["make"], cwd=build_dir)
 
-        # Continue with standard build_ext
-        super().run()
+    # Continue with standard build_ext
+    super().run()
 
 
 setup(
     name="fastalign-wrapper",
-    version="0.1",
-    packages=find_packages(),  # will find your 'fastalign' folder
+    version="1.0.0",
+    packages=find_packages(),
     include_package_data=True,
     cmdclass={"build_ext": BuildFastAlign},
     install_requires=[],
